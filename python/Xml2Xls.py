b708252a884ab74b3cf370d4e8fb42156b794dbf
caixiaoyue <caixiaoyue@uuchat.io>
2020-03-15 18:39:58 +0800

    fix: Fix path error in Windows

--- python/Xml2Xls.py
+++ python/Xml2Xls.py
@@ -38,70 +38,69 @@ def convertToMultipleFiles(fileDir, targetDir):
         valuesDirs = [di for di in dirnames if di.startswith("values")]
         for dirname in valuesDirs:
             workbook = pyExcelerator.Workbook()
-            for _, _, filenames in os.walk(fileDir+'/'+dirname):
+            for _, _, filenames in os.walk(os.path.join(fileDir, dirname)):
                 xmlFiles = [fi for fi in filenames if fi.endswith(".xml")]
                 for xmlfile in xmlFiles:
                     ws = workbook.add_sheet(xmlfile)
-                    path = fileDir+'/'+dirname+'/' + xmlfile
+                    path = os.path.join(fileDir, dirname, xmlfile)
                     (keys, values) = XmlFileUtil.getKeysAndValues(path)
                     for keyIndex in range(len(keys)):
                         key = keys[keyIndex]
                         value = values[keyIndex]
                         ws.write(keyIndex, 0, key)
                         ws.write(keyIndex, 1, value)
-            filePath = destDir + "/" + getCountryCode(dirname) + ".xls"
+            filePath = os.path.join(destDir, getCountryCode(dirname) + ".xls")
             workbook.save(filePath)
     print "Convert %s successfully! you can see xls file in %s" % (
         fileDir, destDir)
 
 
-def convertToSingleFile(fileDir, targetDir):
-    destDir = genDestDir(targetDir)
-    defaultKeyValues = {}
+def convertToSingleFile(file_dir, target_dir):
+    dest_dir = genDestDir(target_dir)
+    default_key_values = {}
 
-    for _, dirnames, _ in os.walk(fileDir):
-        if "values" in dirnames:
-            dirnames.remove("values")
-            dirnames.insert(0, "values")
-        valuesDirs = [di for di in dirnames if di.startswith("values")]
-        for dirname in valuesDirs:
-            for _, _, filenames in os.walk(fileDir+'/'+dirname):
-                xmlFiles = [fi for fi in filenames if fi.endswith(".xml")]
-                for xmlfile in xmlFiles:
-                    fileName = xmlfile.replace(".xml", "")
-                    filePath = destDir + "/" + fileName + ".xls"
-                    if not os.path.exists(filePath):
+    for _, dir_names, _ in os.walk(file_dir):
+        if "values" in dir_names:
+            dir_names.remove("values")
+            dir_names.insert(0, "values")
+        values_dirs = [di for di in dir_names if di.startswith("values")]
+        for dir_name in values_dirs:
+            for _, _, filenames in os.walk(os.path.join(file_dir, dir_name)):
+                xml_files = [fi for fi in filenames if fi.endswith(".xml")]
+                for xml_file in xml_files:
+                    file_name = xml_file.replace(".xml", "")
+                    file_path = os.path.join(dest_dir, file_name + ".xls")
+                    if not os.path.exists(file_path):
                         workbook = pyExcelerator.Workbook()
-                        ws = workbook.add_sheet(fileName)
+                        ws = workbook.add_sheet(file_name)
                         index = 0
-                        for dirname in dirnames:
+                        for dir_name in dir_names:
                             if index == 0:
                                 ws.write(0, 0, 'keyName')
-                            countryCode = getCountryCode(dirname)
-                            ws.write(0, index+1, countryCode)
+                            country_code = getCountryCode(dir_name)
+                            ws.write(0, index+1, country_code)
 
-                            path = fileDir+'/'+dirname+'/' + xmlfile
+                            path = os.path.join(file_dir, dir_name, xml_file)
                             (keys, values, keyValues) = XmlFileUtil.getKeysAndValues(path)
                             x = 0
-                            if dirname == "values":
-                                defaultKeyValues = keyValues
-                            for key, value in defaultKeyValues.items():
-                                relValue = keyValues[key] if key in keyValues else ""
-                                if (index == 0):
+                            if dir_name == "values":
+                                default_key_values = keyValues
+                            for key, value in default_key_values.items():
+                                rel_value = keyValues[key] if key in keyValues else ""
+                                if index == 0:
                                     ws.write(x+1, 0, key)
-                                    ws.write(x+1, 1, relValue)
+                                    ws.write(x+1, 1, rel_value)
                                 else:
-                                    ws.write(x+1, index + 1, relValue)
+                                    ws.write(x+1, index + 1, rel_value)
                                 x += 1
                             index += 1
-                        workbook.save(filePath)
+                        workbook.save(file_path)
     print "Convert %s successfully! you can see xls file in %s" % (
-        fileDir, destDir)
+        file_dir, dest_dir)
 
 
 def genDestDir(targetDir):
-    destDir = targetDir + "/xml-files-to-xls_" + \
-        time.strftime("%Y%m%d_%H%M%S")
+    destDir = os.path.join(targetDir, "xml-files-to-xls_" + time.strftime("%Y%m%d_%H%M%S"))
     if not os.path.exists(destDir):
         os.makedirs(destDir)
     return destDir
